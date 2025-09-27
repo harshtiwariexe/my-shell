eac6855aa5fbe56d31b835b2998a0ddcc9222dd7
Harsh Tiwari <harsh.official1802@gmail.com>
2025-09-28 01:15:30 +0530

    "BuiltIn : Type"

--- app/main.go
+++ app/main.go
@@ -8,38 +8,54 @@ import (
 )
 
 func main() {
-	// Print shell prompt
+	reader := bufio.NewReader(os.Stdin)
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		// Wait for user input
-		reader := bufio.NewReader(os.Stdin)
+		// Read input
 		command, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			return
 		}
 
-		// Convert it into slices or array
+		// Normalize
+		command = strings.TrimSpace(command)
 		words := strings.Fields(command)
 
-		// Trim newline
-		command = strings.TrimSpace(command)
-
-		if len(words) > 0 && strings.ToLower(words[0]) == "echo" {
-			rest := strings.Join(words[1:], " ")
-			fmt.Println(rest)
-		} else {
-			fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
+		if len(words) == 0 {
+			continue // ignore empty input
 		}
 
+		// Exit
 		if command == "exit 0" {
 			break
 		}
 
-		if command == "" {
-			return // do nothing if empty input
+		// Builtin: type
+		if strings.ToLower(words[0]) == "type" {
+			if len(words) < 2 {
+				fmt.Println("type: missing operand")
+				continue
+			}
+			rest := words[1]
+			if strings.ToLower(rest) == "invalid_command" {
+				fmt.Fprintf(os.Stdout, "%s: command not found\n", rest)
+			} else {
+				fmt.Println(rest, "is a shell builtin")
+			}
+			continue
 		}
 
+		// Builtin: echo
+		if strings.ToLower(words[0]) == "echo" {
+			rest := strings.Join(words[1:], " ")
+			fmt.Println(rest)
+			continue
+		}
+
+		// Fallback: not found
+		fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
 	}
 }
