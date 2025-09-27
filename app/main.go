67cb88250cc46abc9610e2e45989b40e62fe24da
Harsh Tiwari <harsh.official1802@gmail.com>
2025-09-27 20:04:25 +0530

    REPL

--- app/main.go
+++ app/main.go
@@ -9,22 +9,25 @@ import (
 
 func main() {
 	// Print shell prompt
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	reader := bufio.NewReader(os.Stdin)
-	command, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		return
+		// Wait for user input
+		reader := bufio.NewReader(os.Stdin)
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			return
+		}
+
+		// Trim newline
+		command = strings.TrimSpace(command)
+
+		if command == "" {
+			return // do nothing if empty input
+		}
+
+		fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
+
 	}
-
-	// Trim newline
-	command = strings.TrimSpace(command)
-
-	if command == "" {
-		return // do nothing if empty input
-	}
-
-	fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
 }
